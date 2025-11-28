d65444c99fae41e4eaa25c4d788452e37fa4b060
unknown <abdoubakka@gmail.com>
2025-11-28 18:48:43 +0100

    Update axios baseURL for Railway deployment

--- src/api/axiosConfig.ts
+++ src/api/axiosConfig.ts
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000/api',
+   baseURL: 'https://morocancryptobackend-production.up.railway.app/api',
   headers: {
     'Content-Type': 'application/json',
   },
